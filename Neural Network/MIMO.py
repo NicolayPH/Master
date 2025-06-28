4bdd5deb3c681950af6f483e0aec9858e059551a
NicolayPH <nicolay.p.hansen@gmail.com>
2025-06-28 22:15:14 +0200

    Add files via upload

--- Neural Network/MIMO.py
+++ Neural Network/MIMO.py
@@ -6,6 +6,7 @@ from sklearn import metrics
 import matplotlib.pyplot as plt
 import time
 import json
+import csv
 
 from sklearn.model_selection import StratifiedKFold
 from tensorflow.keras.layers import Dense, Activation, Dropout, InputLayer, BatchNormalization
@@ -18,58 +19,63 @@ from tensorflow.keras.optimizers import Adam
 
 from bayes_opt import BayesianOptimization
 
-matrices = []
-current_matrix = []
+#Creating a function that extracts the data
 
-with open('PreparedTrainingHydrogen.csv', 'r') as f:
-    for line in f:
-        line_stripped = line.strip()
-        if line_stripped == '':
-            matrices.append(current_matrix)
-            current_matrix = []
-        else:
-            current_matrix.append([float(element) for element in line_stripped.split(',')])
-    matrices.append(current_matrix)
+def obtainDataFromFile(component):
+    matrices = []
+    current_matrix = []
 
+    filename = "PreparedTraining" + component + ".csv"
 
-x_input = np.array(matrices[0])
-validation_input = np.array(matrices[1])
-
-
-label = np.array(matrices[2])
-validation_label = np.array(matrices[3])
-
-x_input = x_input.T
-label = label.T
+    with open(filename, 'r') as f:
+        for line in f:
+            line_stripped = line.strip()
+            if line_stripped == '':
+                matrices.append(current_matrix)
+                current_matrix = []
+            else:
+                current_matrix.append([float(element) for element in line_stripped.split(',')])
+        matrices.append(current_matrix)
 
 
-label_max, label_min = label.max(), label.min()
-print("Label_max:", label_max)
-scaled_label = (label-label_min)/(label_max-label_min)
+    x_input = np.array(matrices[0])
+    validation_input = np.array(matrices[1])
 
 
+    label = np.array(matrices[2])
+    validation_label = np.array(matrices[3])
 
-validation_input = validation_input.T
-validation_label = validation_label.T
+    x_input = x_input.T
+    label = label.T
 
 
-validation_label_max, validation_label_min = label_max, label_min
-validated_labeled_output = (validation_label-validation_label_min)/(validation_label_max-validation_label_min)
+    label_max, label_min = label.max(), label.min()
+    print("Label_max:", label_max)
+    scaled_label = (label-label_min)/(label_max-label_min)
 
 
 
-split_index_input = len(validation_input) // 2
-dev_input = validation_input[:split_index_input]
-test_input = validation_input[split_index_input:]
-
-split_index_label = len(validated_labeled_output) // 2
-dev_label = validated_labeled_output[:split_index_label]
-test_label = validated_labeled_output[split_index_label:]
+    validation_input = validation_input.T
+    validation_label = validation_label.T
 
 
-print(x_input.shape)
-print(label.shape)
-print(len(scaled_label.flatten()) + len(validated_labeled_output.flatten()))
+    validation_label_max, validation_label_min = label_max, label_min
+    validated_labeled_output = (validation_label-validation_label_min)/(validation_label_max-validation_label_min)
+
+
+
+    split_index_input = len(validation_input) // 2
+    dev_input = validation_input[:split_index_input]
+    test_input = validation_input[split_index_input:]
+
+    split_index_label = len(validated_labeled_output) // 2
+    dev_label = validated_labeled_output[:split_index_label]
+    test_label = validated_labeled_output[split_index_label:]
+    return x_input, scaled_label, dev_input, test_input, dev_label, test_label
+
+
+component = "CarbonDioxide"
+x_input, scaled_label, dev_input, test_input, dev_label, test_label = obtainDataFromFile(component)
 
 
 def generate_model(dropout, neuronPct, neuronShrink):
@@ -98,7 +104,7 @@ def generate_model(dropout, neuronPct, neuronShrink):
         #Decrease the remaining neuron count after each layer
         neuronCount = int(neuronCount * neuronShrink)
 
-    model.add(Dense(label.shape[1], activation = 'linear')) #output
+    model.add(Dense(scaled_label.shape[1], activation = 'linear')) #output
     return model
 
 
@@ -134,7 +140,7 @@ def evaluate_network(dropout, learning_rate, neuronPct, neuronShrink):
     mdev = statistics.pstdev(mean_benchmark)
 
     return -m1
-'''
+
 
 #Next step is to optimize this process
 pbounds = {'dropout': (0.0, 0.499),
@@ -161,7 +167,7 @@ def print_optimized_variables(component, init_points = 10, n_iter = 80):
     with open(filename, 'w') as file:
         json.dump(optimizer.max, file, indent = 4)
 
-print_optimized_variables("Hydrogen")
+#print_optimized_variables("Hydrogen")
 
 
 def create_optimized_model(filename):
@@ -184,34 +190,134 @@ def create_optimized_model(filename):
     return opt_model
 
 
-opt_model = create_optimized_model("optimizer_max_Hydrogen.json")
-'''
-'''
-opt_model = generate_model(dropout = 0.13755646002041275, neuronPct = 0.5724888941223814, neuronShrink = 0.1284396537855462)
-opt_model.compile(loss = 'mean_squared_error', optimizer = Adam(learning_rate = 0.03929355991914982))
-monitor = EarlyStopping(monitor = 'val_loss', min_delta = 1e-3, patience = PATIENCE, verbose = 0, mode = 'auto', restore_best_weights = True)
+#opt_model = create_optimized_model("optimizer_max_Hydrogen.json")
 
-#Train on the bootstrap sample
-opt_model.fit(x_input, scaled_label, validation_data = (dev_input, dev_label), callbacks = [monitor], verbose = 0, epochs = EPOCHS, shuffle = True, batch_size = 128)
-#print(opt_model(test_input))
+#Optimal Bayesian parameters derived from the optimalization
+#The structure is [Dropout, Neuron Percentage, Neuron Shrinkage, Learning Rate]
+opt_params = {"Hydrogen": [[0.07297661940550304, 0.8984631275262264, 0.29169206875647286, 0.010163449147197457],
+                           [0.016636186159798092, 0.1149877049819497, 0.6027536506425499, 0.0023145462085277456],
+                           [0.0694469621218864, 0.1936504719634992,	0.35251410992317794, 0.015378057562614811]],
+              "CarbonDioxide": [[0.369348991507222, 0.16879766605746643, 0.74371114040222, 0.09838879439821609],
+                                 [0.17947256838745337, 0.7275879010741872, 0.5257213908147264, 0.05069429027147701],
+                                 [0.13755646002041275, 0.5724888941223814, 0.1284396537855462, 0.03929355991914982]],
+              "Nitrogen": [[0.3218303736553769, 0.682644371293227, 0.2642750657580455, 0.012522076894588363],
+                           [0.00477788338732836, 0.4222469957852342, 0.030467800804570976, 0.08206812375152706],
+                           [0.499, 0.19903107317958418,	0.474482349728461, 0.1]]
+}
 
-#print(err)
-plt.scatter(test_label, opt_model(test_input))
-plt.xlabel('Normalized True Values')
-plt.ylabel('Normalized Predictions')
-plt.title("Carbon Dioxide")
-plt.plot([0,1], [0,1], 'k--')
-plt.savefig("CarbonDioxide_MIMO_5000.pdf")
-plt.show()
-'''
+#Want to create plots, so a function is defined
+def plot_subplots(ax, true_values, pred_values, legend):
+    ax.scatter(true_values.flatten(), pred_values.flatten(), s=2, alpha=0.5)
+    ax.text(0.05, 0.95, f"Initalized neurons = {legend}", transform = ax.transAxes, fontsize = 10, verticalalignment = 'top', bbox = dict(boxstyle='round', facecolor='white', edgecolor='black', alpha=0.8))
+    ax.plot([true_values.min(), true_values.max()], [true_values.min(), true_values.max()], 'k--', lw=1)
+    ax.set_xlabel(r"Normalized True Values")
+    ax.grid(True)
+
+#Plot histogram of data 
+def plot_histogram(component):
+    #Retrieving data from file
+    x_input, scaled_label, dev_input, test_input, dev_label, test_label = obtainDataFromFile(component)
+
+    component_flatten = np.append(scaled_label.flatten(), dev_label.flatten())
+    component_flatten = np.append(component_flatten, test_label.flatten())
+
+    #Carbon Dioxide is called "CarbonDioxide", so want to change that in the labeling
+    if component == "CarbonDioxide":
+        component = "Carbon Dioxide"
+
+    plt.figure()
+    plt.hist(component_flatten, bins = 50, color = 'skyblue', edgecolor = 'black')
+    plt.title(component + " Density Distribution")
+    plt.xlabel('Normalized Density Value')
+    plt.ylabel('Frequency')
+    plt.grid()
+    plt.tight_layout()
+    plt.savefig(component + " Histogram.pdf", bbox_inches = "tight")
+    plt.show()
+
+#Want to plot with the same style as in LaTeX
+plt.rcParams.update({
+    "text.usetex": False,
+    "font.family": "serif",
+    "font.serif": ["DejaVu Serif"],
+    "font.size": 12,
+})  
+
+#Creating functions that write to and read from files, respectively
+def writeValuesToFile(filename, pred_values):
+    with open(filename, "w", newline = '') as f:
+        writeToCSV = csv.writer(f)
+        for el in pred_values:
+            writeToCSV.writerow(el)
+
+def getValuesFromFile(filename):
+    arr = np.loadtxt(filename, delimiter = ',')
+    return arr
+
+#Now, we can start saving the data
+componentList = list(opt_params.keys())
+print(componentList)
 
 
-component_flatten = np.append(scaled_label.flatten(), validated_labeled_output.flatten())
-plt.figure()
-plt.hist(component_flatten, bins = 50, color = 'skyblue', edgecolor = 'black')
-plt.title('Hydrogen Density Distribution')
-plt.xlabel('Normalized Density Value')
-plt.ylabel('Frequency')
-plt.grid()
-plt.savefig("Hydrogen Histogram.pdf")
-plt.show()
+#Index, 250 neurons : 0, 1000 neurons : 1 and 5000 neurons :  2
+#Making a dictionary to simplify the labelings later
+indexes = {0: "250", 
+           1: "1000",
+           2: "5000"
+}
+
+
+
+indexList = list(indexes.keys())
+
+def plotAllFigures():
+    for component in componentList:
+        #The data is different for different component
+        x_input, scaled_label, dev_input, test_input, dev_label, test_label = obtainDataFromFile(component)
+        for idx in indexList:
+            opt_model = generate_model(dropout = opt_params[component][idx][0], neuronPct = opt_params[component][idx][1], neuronShrink = opt_params[component][idx][2])
+            opt_model.compile(loss = 'mean_squared_error', optimizer = Adam(learning_rate = opt_params[component][idx][3]))
+            monitor = EarlyStopping(monitor = 'val_loss', min_delta = 1e-3, patience = PATIENCE, verbose = 0, mode = 'auto', restore_best_weights = True)
+
+            #Train on the bootstrap sample
+            opt_model.fit(x_input, scaled_label, validation_data = (dev_input, dev_label), callbacks = [monitor], verbose = 0, epochs = EPOCHS, shuffle = True, batch_size = 128)
+            #print(opt_model(test_input))
+
+
+            predicted_values = opt_model(test_input).numpy()
+
+
+
+            writeValuesToFile(component + "_MIMO_" + indexes[idx] + ".csv", predicted_values)
+
+
+        model_250 = getValuesFromFile(component + "_MIMO_250.csv")
+        model_1000 = getValuesFromFile(component + "_MIMO_1000.csv")
+        model_5000 = getValuesFromFile(component + "_MIMO_5000.csv")
+
+
+        fig, axs = plt.subplots(3, 1, figsize = (6, 8), sharex = True) #figsize=(5, 6), sharex=True, dpi=300)
+
+
+        # Subplots
+        plot_subplots(axs[0], test_label, model_250, "250")
+        plot_subplots(axs[1], test_label, model_1000, "1000")
+        plot_subplots(axs[2], test_label, model_5000, "5000")
+
+        axs[1].set_ylabel(r"Normalized Predictions")
+        axs[2].set_xlabel(r"Normalized True Values")
+
+        for ax in axs[:2]:
+            ax.label_outer()
+
+        #plt.tight_layout(rect=[0, 0, 1, 0.90])
+        plt.savefig(component + "_MIMO.pdf", bbox_inches='tight', dpi = 300)  # For LaTeX import
+        plt.show()
+
+#plotAllFigures()
+
+
+#Want to create the histograms
+for component in componentList:
+    plot_histogram(component)
+
--- Neural Network/MISO.py
+++ Neural Network/MISO.py
@@ -6,6 +6,7 @@ from sklearn import metrics
 import matplotlib.pyplot as plt
 import time
 import json
+import csv
 
 from sklearn.model_selection import StratifiedKFold
 from tensorflow.keras.layers import Dense, Activation, Dropout, InputLayer, BatchNormalization
@@ -18,32 +19,6 @@ from tensorflow.keras.optimizers import Adam
 
 from bayes_opt import BayesianOptimization
 
-matrices = []
-current_matrix = []
-
-with open('PreparedTrainingNitrogen.csv', 'r') as f:
-    for line in f:
-        line_stripped = line.strip()
-        if line_stripped == '':
-            matrices.append(current_matrix)
-            current_matrix = []
-        else:
-            current_matrix.append([float(element) for element in line_stripped.split(',')])
-    matrices.append(current_matrix)
-
-
-x_input = np.array(matrices[0])
-validation_input = np.array(matrices[1])
-
-
-label = np.array(matrices[2])
-validation_label = np.array(matrices[3])
-
-
-
-
-x_input = x_input.T
-label = label.T
 
 #For the MISO approach, each input layer needs to be added the numbers 1-201. 
 #Thus, each row must be repeated 201 times
@@ -57,33 +32,69 @@ def add_grid_numbers(input):
         final_input = np.hstack((repeated_input, z_column))
         return final_input
 
+#Creating a function that extracts the data
 
-label_max, label_min = label.max(), label.min()
-scaled_label = (label-label_min)/(label_max-label_min)
+def obtainDataFromFile(filename):
+    matrices = []
+    current_matrix = []
 
-
-validation_input = validation_input.T
-validation_label = validation_label.T
-
-x_input = add_grid_numbers(x_input)
-validation_input = add_grid_numbers(validation_input)
-
-
-validation_label_max, validation_label_min = label_max, label_min
-validated_labeled_output = (validation_label-validation_label_min)/(validation_label_max-validation_label_min)
-
-
-validated_labeled_output = validated_labeled_output.flatten().reshape(-1, 1)
-scaled_label = scaled_label.flatten().reshape(-1,1)
+    with open(filename, 'r') as f:
+        for line in f:
+            line_stripped = line.strip()
+            if line_stripped == '':
+                matrices.append(current_matrix)
+                current_matrix = []
+            else:
+                current_matrix.append([float(element) for element in line_stripped.split(',')])
+        matrices.append(current_matrix)
 
 
-split_index_input = len(validation_input) // 2
-dev_input = validation_input[:split_index_input]
-test_input = validation_input[split_index_input:]
+    x_input = np.array(matrices[0])
+    validation_input = np.array(matrices[1])
 
-split_index_label = len(validated_labeled_output) // 2
-dev_label = validated_labeled_output[:split_index_label]
-test_label = validated_labeled_output[split_index_label:]
+
+    label = np.array(matrices[2])
+    validation_label = np.array(matrices[3])
+
+    x_input = x_input.T
+    label = label.T
+
+
+    label_max, label_min = label.max(), label.min()
+    scaled_label = (label-label_min)/(label_max-label_min)
+
+
+    validation_input = validation_input.T
+    validation_label = validation_label.T
+
+    x_input = add_grid_numbers(x_input)
+    validation_input = add_grid_numbers(validation_input)
+
+
+    validation_label_max, validation_label_min = label_max, label_min
+    validated_labeled_output = (validation_label-validation_label_min)/(validation_label_max-validation_label_min)
+
+
+    validated_labeled_output = validated_labeled_output.flatten().reshape(-1, 1)
+    scaled_label = scaled_label.flatten().reshape(-1,1)
+
+    split_index_input = len(validation_input) // 2
+    dev_input = validation_input[:split_index_input]
+    test_input = validation_input[split_index_input:]
+
+    split_index_label = len(validated_labeled_output) // 2
+    dev_label = validated_labeled_output[:split_index_label]
+    test_label = validated_labeled_output[split_index_label:]
+    return x_input, scaled_label, dev_input, test_input, dev_label, test_label
+
+
+filename = 'PreparedTrainingCarbonDioxide.csv' 
+x_input, scaled_label, dev_input, test_input, dev_label, test_label = obtainDataFromFile(filename)
+print(scaled_label)
+
+print(x_input.shape)
+print(scaled_label.shape)
+print(dev_label.shape)
 
 
 def generate_model(dropout, neuronPct, neuronShrink):
@@ -202,29 +213,122 @@ def create_optimized_model(filename):
 
 opt_model = create_optimized_model("optimizer_max_MISO_Hydrogen.json")
 '''
-opt_model = generate_model(dropout = 0.11200877459831117, neuronPct = 0.5725772716455936, neuronShrink = 0.953133835030213)
-opt_model.compile(loss = 'mean_squared_error', optimizer = Adam(learning_rate = 0.052770534789438155))
-monitor = EarlyStopping(monitor = 'val_loss', min_delta = 1e-3, patience = PATIENCE, verbose = 0, mode = 'auto', restore_best_weights = True)
 
-#Train on the bootstrap sample
-opt_model.fit(x_input, scaled_label, validation_data = (dev_input, dev_label), callbacks = [monitor], verbose = 0, epochs = EPOCHS, shuffle = True, batch_size = 128)
-#print(opt_model(test_input))
 
-plt.scatter(test_label, opt_model(test_input))
-plt.xlabel('Normalized True Values')
-plt.ylabel('Normalized Predictions')
-plt.title("Nitrogen")
-plt.plot([0,1], [0,1], 'k--')
-plt.savefig("Nitrogen_MISO_250.pdf")
-plt.show()
+#Optimal Bayesian parameters derived from the optimalization
+#The structure is [Dropout, Neuron Percentage, Neuron Shrinkage, Learning Rate]
+opt_params = {"Hydrogen": [[0.1848743322126204, 0.4743176979592553, 0.778096724897786, 0.04027761491347523],
+                          [0.16837564583758236, 0.5416705506200448,	0.3721733266564638, 0.06566139574322541],
+                            [0.19798696411043, 0.4250025692592619,	0.6883673053927919, 0.0538816734003357]],
+              "CarbonDioxide": [[0.432914747407662, 0.717145422776755, 0.3341282059666561, 0.0800159045996249],
+                               [0.3976476431269369, 0.7964735689412817, 0.7283293722152454, 0.08915556939599203]],
+              "Nitrogen": [[0.11200877459831117, 0.5725772716455936, 0.953133835030213, 0.052770534789438155],
+                          [0.09284031889436505, 0.42654441107643976, 0.08369921893509626, 0.11970898251567202],
+                          [0.3525897295394852, 0.7794703419904637, 0.3468746147832975, 0.05550026809326039]]
+}
 
-'''
-nitrogen_flat = scaled_label.flatten()
-plt.figure()
-plt.hist(nitrogen_flat, bins = 50, color = 'skyblue', edgecolor = 'black')
-plt.title('Carbon Dioxide Density Distribution')
-plt.xlabel('Density Value')
-plt.ylabel('Frequency')
-plt.grid()
-plt.show()
-'''
+
+#Want to create plots, so a function is defined
+def plot_subplots(ax, true_values, pred_values, legend):
+    ax.scatter(true_values.flatten(), pred_values.flatten(), s=2, alpha=0.5)
+    ax.text(0.05, 0.95, f"Initalized neurons = {legend}", transform = ax.transAxes, fontsize = 10, verticalalignment = 'top', bbox = dict(boxstyle='round', facecolor='white', edgecolor='black', alpha=0.8))
+    ax.plot([true_values.min(), true_values.max()], [true_values.min(), true_values.max()], 'k--', lw=1)
+    ax.set_xlabel(r"Normalized True Values")
+    ax.grid(True)
+
+#Want to plot with the same style as in LaTeX
+plt.rcParams.update({
+    "text.usetex": False,
+    "font.family": "serif",
+    "font.serif": ["DejaVu Serif"],
+    "font.size": 12,
+})  
+
+#Creating functions that write to and read from files, respectively
+def writeValuesToFile(filename, pred_values):
+    with open(filename, "w", newline = '') as f:
+        writeToCSV = csv.writer(f)
+        for el in pred_values:
+            writeToCSV.writerow(el)
+
+def getValuesFromFile(filename):
+    arr = np.loadtxt(filename, delimiter = ',')
+    return arr
+
+#Now, we can start saving the data
+componentList = list(opt_params.keys())
+print(componentList)
+
+
+#Index, 250 neurons : 0, 1000 neurons : 1 and 5000 neurons :  2
+#Making a dictionary to simplify the labelings later
+indexes = {0: "250", 
+           1: "1000",
+           2: "5000"
+}
+
+
+indexList = list(indexes.keys())
+
+def plotAllFigures():
+    for component in componentList:
+        #The data is different for different component
+        preparedTraining_filename = "PreparedTraining" + component + ".csv"
+        x_input, scaled_label, dev_input, test_input, dev_label, test_label = obtainDataFromFile(preparedTraining_filename)
+        if component == "CarbonDioxide":   #Since the data for 5000 isn't available for Carbon Dioxide
+            indexList = [0, 1]
+        else: 
+            indexList = list(indexes.keys())
+        for idx in indexList:
+            opt_model = generate_model(dropout = opt_params[component][idx][0], neuronPct = opt_params[component][idx][1], neuronShrink = opt_params[component][idx][2])
+            opt_model.compile(loss = 'mean_squared_error', optimizer = Adam(learning_rate = opt_params[component][idx][3]))
+            monitor = EarlyStopping(monitor = 'val_loss', min_delta = 1e-3, patience = PATIENCE, verbose = 0, mode = 'auto', restore_best_weights = True)
+
+            #Train on the bootstrap sample
+            opt_model.fit(x_input, scaled_label, validation_data = (dev_input, dev_label), callbacks = [monitor], verbose = 0, epochs = EPOCHS, shuffle = True, batch_size = 128)
+            #print(opt_model(test_input))
+
+
+            predicted_values = opt_model(test_input).numpy()
+
+
+
+            writeValuesToFile(component + "_MISO_" + indexes[idx] + ".csv", predicted_values)
+
+
+        model_250 = getValuesFromFile(component + "_MISO_250.csv")
+        model_1000 = getValuesFromFile(component + "_MISO_1000.csv")
+
+        
+        
+        if component != "CarbonDioxide":
+            model_5000 = getValuesFromFile(component + "_MISO_5000.csv")
+            fig, axs = plt.subplots(3, 1, figsize = (6, 8), sharex = True) #figsize=(5, 6), sharex=True, dpi=300)
+        else:
+            fig, axs = plt.subplots(2, 1, figsize = (6, 8), sharex = True) #figsize=(5, 6), sharex=True, dpi=300)
+
+        # Subplots
+        plot_subplots(axs[0], test_label, model_250, "250")
+        plot_subplots(axs[1], test_label, model_1000, "1000")
+        if component != "CarbonDioxide":
+            plot_subplots(axs[2], test_label, model_5000, "5000")
+            axs[1].set_ylabel(r"Normalized Predictions")
+            axs[2].set_xlabel(r"Normalized True Values")
+        else:
+            #axs[1].set_ylabel(r"Normalized Predictions")
+            fig.text(0.04, 0.5, 'Normalized Predictions', va='center', rotation='vertical', fontsize=12)
+            axs[1].set_xlabel(r"Normalized True Values")
+
+        
+
+        
+        for ax in axs[:2]:
+            ax.label_outer()
+
+        #plt.tight_layout(rect=[0, 0, 1, 0.90])
+        plt.savefig(component + "_MISO.pdf", bbox_inches='tight', dpi = 300)  # For LaTeX import
+        plt.show()
+
+plotAllFigures()
+
+
